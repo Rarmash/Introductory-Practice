f116cc56f9b42e9087a446bfcab423e798b2b5f8
Andrew <62994051+Rarmash@users.noreply.github.com>
2023-02-14 10:31:18 +0300

    FASTFIX

--- Тема 0/task1.py
+++ Тема 0/task1.py
@@ -3,13 +3,10 @@
 '''
 
 def func1(n):
-    if n > 0:
-        c = 0
-        for i in range(1, n + 1):
-            if n % i == 0:
-                c += 1
-        return f'{n} - простое число' if c == 2 else f'{n} - составное число'
-    else:
-        return f'{n} не является натуральным числом'
+    c = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            c += 1
+    return True if c == 2 else False
 
 print(func1(int(input())))
--- Тема 0/task3.py
+++ Тема 0/task3.py
@@ -6,13 +6,18 @@ list2 = [1, 2, -3, 4, 5, -1, -1, 2, 3, -3, 4]
 
 def func3(list2):
     c = 1
-    maxx = 0
+    a = []
+    maxa = []
     for i in range(1, len(list2)):
         if list2[i-1] < list2[i]:
+            if c == 1:
+                a.append(list2[i-1])
             c += 1
+            a.append(list2[i])
         else:
-            maxx = c
             c = 1
-    return f'Наибольшая серия подряд идущих элементов - {maxx}'
+            maxa = a
+            a = []
+    return f'{maxa}'
 
 print(func3(list2))
