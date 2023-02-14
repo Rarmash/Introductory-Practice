7b354054b19101f25cb6fcaa0c7e4095cc8c4252
Andrew <62994051+Rarmash@users.noreply.github.com>
2023-02-14 10:05:04 +0300

    Update task2.py

--- Тема 0/task2.py
+++ Тема 0/task2.py
@@ -6,9 +6,13 @@
 list1 = [1, 2, -3, 4, 5, -1, -1, 2, 3, -3, 4]
 
 def func2(list1):
-    c = 0
-    for i in range(1, len(list1)-1):
-        c += list1[i]
-    return c
+    x1, x2 = None, None
+    for i in range(1, len(list1)//2+1):
+        if list1[i-1] < 0 and x1 == None:
+            x1 = i - 1
+        if list1[-1] < 0 and x2 == None:
+            x2 = i
+    l = list1[x1+1:x2]
+    return f'{sum(l), x1, x2}\n{sum(list1[3:-2])}'
 
 print(func2(list1))
